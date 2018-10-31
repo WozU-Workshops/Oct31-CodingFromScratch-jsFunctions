4cb965111b5436f979130d43f06e56d199b17d80
Cassandra Matos <cassandra@matosmendez.com>
2018-10-31 12:35:13 -0700

    upload code base

---
+++ script.js
@@ -0,0 +1,36 @@
+// FUNCTION SYNTAX 
+// function nameOfFunction(parameters){
+//     functionality;
+// }
+
+
+
+// PRACTICE TIME
+// Write a function that takes in two string parameters and adds them together and returns the concatenation of those strings.
+
+
+
+// CHALLENGE TIME
+// Write a function that calculates someone's age based on the following parameters: month of birth, day of birth, year of birth, and current month, day, and year.
+
+
+
+// DID YOU KNOW? TIME
+// Let's look at a function that takes in a function as a parameter
+// function addStudent(id, refreshCallback)
+// {
+//     refreshCallback();  
+// }
+
+// function refreshStudentList() {
+//     console.log("You have been successfully enrolled");
+// }
+
+// addStudent(1, refreshStudentList);
+
+
+
+// DOM & FUNCTION TIME
+// Write a function that takes in the user's information on the HTML form
+// and wishes the user a spooky halloween. For example, the form asks for the user's first and last name, the message displayed should say "Have a spooky halloween, <first> <last>!" and display the hidden image (hint: use the javascript HTML DOM className property to make the image show - you can use the property as so: element.className='theClassNameYouWantToAdd')
+
